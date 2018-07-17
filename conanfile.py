4cd1210e1e51faa1bd2ca0deb897c18f536aff57
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-07-18 02:41:14 +0300

    Clean

--- conanfile.py
+++ conanfile.py
@@ -28,47 +28,23 @@ class XercesConan(ConanFile):
         build_type = "RelWithDebInfo" if self.settings.build_type == "Release" else "Debug"
         cmake = CMake(self, build_type=build_type)
         cmake.verbose = True
+        #
         cmake.definitions["network:BOOL"] = "OFF"
         cmake.definitions["transcoder"] = "icu"
         cmake.definitions["message-loader"] = "inmemory"
         cmake.definitions["BUILD_SHARED_LIBS:BOOL"] = "OFF"
         if self.settings.os == "Windows":
             cmake.definitions["xmlch-type"] = "wchar_t"
+        if self.settings.os == "Linux":
+            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE:BOOL"] = "ON"
+        #
         cmake.configure()
         cmake.build()
         cmake.install()
 
     def package(self):
         self.copy("*xerces-c.pdb", dst="bin", keep_path=False)
-        return
-        # CMake scripts
-        self.copy("FindGTest.cmake", dst=".", src=".", keep_path=False)
-        self.copy("FindGMock.cmake", dst=".", src=".", keep_path=False)
-        # Headers
-        self.copy("*.h", dst="include", src="src/googletest/include", keep_path=True)
-        self.copy("*.h", dst="include", src="src/googlemock/include", keep_path=True)
-        # Libraries
-        self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*.lib", dst="lib", keep_path=False)
-        # PDB
-        self.copy("*gtest.pdb", dst="bin", keep_path=False)
-        self.copy("*gmock.pdb", dst="bin", keep_path=False)
-        self.copy("*gtest_main.pdb", dst="bin", keep_path=False)
-        self.copy("*gmock_main.pdb", dst="bin", keep_path=False)
-        self.copy("*gtestd.pdb", dst="bin", keep_path=False)
-        self.copy("*gmockd.pdb", dst="bin", keep_path=False)
-        self.copy("*gtest_maind.pdb", dst="bin", keep_path=False)
-        self.copy("*gmock_maind.pdb", dst="bin", keep_path=False)
 
     def package_info(self):
-        return
-        self.cpp_info.libs = ["gmock_main"] if self.settings.build_type == "Release" else ["gmock_maind"]
-        self.cpp_info.defines = ["GTEST_LANG_CXX11"]
-        #
-        if self.settings.os == "Linux":
-            if self.settings.build_type == "Release":
-                self.cpp_info.libs.extend(["gmock", "gtest"])
-            else:
-                self.cpp_info.libs.extend(["gmockd", "gtestd"])
-            self.cpp_info.libs.append("pthread")
+        self.cpp_info.libs = tools.collect_libs(self)
 
