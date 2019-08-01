eaaea269ac89b3fc7eed58132d87052d69c67138
d0nc1h0t <>
2019-08-01 16:20:01 +0300

    Version 3.2.2+4

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ def get_safe(options, name):
 
 class XercesConan(ConanFile):
     name = "xerces-c"
-    version = "3.2.2+3"
+    version = "3.2.2+4"
     license = "Apache License v2.0"
     description = "Xerces-C++ XML parser"
     url = "https://github.com/odant/conan-xerces-c"
