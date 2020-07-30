af8675666f218026e86e882cd9e6ed529e022fe0
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-07-30 21:18:29 +0300

    Upgrade CI

--- build.py
+++ build.py
@@ -1,55 +1,24 @@
-import platform, os
-from copy import deepcopy
+# Build Conan package
+# Dmitriy Vetutnev, ODANT, 2020
+
+
 from conan.packager import ConanMultiPackager
+import os
 
 
-# Common settings
-username = "odant" if "CONAN_USERNAME" not in os.environ else None
-# Windows settings
-visual_versions = ["15", "16"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
-visual_runtimes = ["MD", "MDd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ else None
-dll_sign = False if "CONAN_DISABLE_DLL_SIGN" in os.environ else True
-
-
-def add_dll_sign(builds):
-    result = []
-    for settings, options, env_vars, build_requires, reference in builds:
-        options = deepcopy(options)
-        opt_name = "icu:dll_sign"
-        options[opt_name] = dll_sign
-        result.append([settings, options, env_vars, build_requires, reference])
-    return result
-
-
-def filter_libcxx(builds):
-    result = []
-    for settings, options, env_vars, build_requires, reference in builds:
-        if settings["compiler.libcxx"] == "libstdc++11":
-            result.append([settings, options, env_vars, build_requires, reference])
-    return result
+env_pure_c = os.getenv("CONAN_PURE_C", True)
+pure_c = True if str(env_pure_c).lower() != "false" else False
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(
-        username=username,
-        visual_versions=visual_versions,
-        visual_runtimes=visual_runtimes,
         exclude_vcvars_precommand=True
     )
-    builder.add_common_builds(pure_c=False)
-    # Adjusting build configurations
-    builds = builder.items
-    if platform.system() == "Windows":
-        builds = add_dll_sign(builds)
-    if platform.system() == "Linux":
-        builds = filter_libcxx(builds)
-    # Replace build configurations
-    builder.items = []
-    for settings, options, env_vars, build_requires, _ in builds:
-        builder.add(
-            settings=settings,
-            options=options,
-            env_vars=env_vars,
-            build_requires=build_requires
-        )
+    builder.add_common_builds(
+        pure_c=pure_c
+    )
+    builder.remove_build_if(
+        lambda build: build.settings.get("compiler.libcxx") == "libstdc++"
+    )
     builder.run()
+
